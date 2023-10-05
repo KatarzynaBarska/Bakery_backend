26fb4e8dca77562d41591057b770902589a7207d
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-10-05 11:30:53 +0200

    Added method: insert,getOne,listAll to seed.record.ts.

---
+++ records/seed.record.ts
@@ -0,0 +1,114 @@
+import {v4 as uuid} from 'uuid';
+import {SeedEntity} from '../types';
+import {pool} from "../utils/db";
+import {FieldPacket} from "mysql2";
+import {BaseRecord} from "./base.record";
+
+type SeedRecordResults = [SeedRecord[], FieldPacket[]];
+
+export class SeedRecord implements SeedEntity {
+    idSeed?: string;
+    name: string;
+    price: number;
+
+    constructor(obj: SeedEntity) {
+        this.idSeed = obj.idSeed;
+        this.name = obj.name;
+        this.price = obj.price;
+    }
+
+    async insert(): Promise<string> {
+        if (!this.idSeed) {
+            this.idSeed = uuid();
+        }
+
+        await pool.execute("INSERT INTO `seeds`(`idSeed`,`name`,`price`) VALUES(:idSeed, :name, :price)", {
+            idSeed: this.idSeed,
+            name: this.name,
+            price: this.price,
+        });
+
+        return this.idSeed;
+    }
+
+    static async listAll(): Promise<SeedRecord[]> {
+        const [results] = await pool.execute("SELECT * FROM `seeds` ORDER BY `name` ASC") as SeedRecordResults;
+        return results.map(obj => new SeedRecord(obj));
+    }
+
+    static async getOne(idSeed: string): Promise<SeedRecord | null> {
+        const [result] = await pool.execute("SELECT * FROM `seeds` WHERE `idSeed` = :idSeed", {
+            idSeed,
+        }) as SeedRecordResults;
+        return result.length === 0 ? null : new SeedRecord(result[0]);
+    }
+
+    async delete(): Promise<void> {
+        await pool.execute("DELETE FROM `seeds` WHERE `idSeed` = :idSeed, ", {
+            idSeed: this.idSeed,
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
