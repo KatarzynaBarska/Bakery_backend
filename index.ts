a94a79a39228cfc35c1b384d4f2db30e2abef700
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-10-06 18:00:56 +0200

    Added routers: base and seed.

--- index.ts
+++ index.ts
@@ -3,17 +3,25 @@ import express, {json} from 'express';
 import {config} from "./config/config";
 import rateLimit from 'express-rate-limit';
 import {handleError} from "./utils/errors";
+import {baseRouter} from "./routers/base";
+import {seedRouter} from "./routers/seed";
 
 const app = express();
 
 app.use(cors({
     origin: config.corsOrigin,
 }));
+
 app.use(json());
+
 app.use(rateLimit({
     windowMs: 5 * 60 * 1000,
     limit: 100,
 }));
+
+app.use('/base', baseRouter);
+app.use('/seed', seedRouter);
+
 
 app.use(handleError);
 
---
+++ routers/seed.ts
@@ -0,0 +1,41 @@
+import {Router} from "express";
+import {SeedRecord} from "../records/seed.record";
+import {ValidationError} from "../utils/errors";
+
+export const seedRouter = Router();
+
+seedRouter
+
+    .get('/', async (req, res) => {
+        const seedsList = await SeedRecord.listAll();
+
+        res.json({
+            seedsList,
+        });
+    })
+
+    .get('/:seedId', async (req, res) => {
+        const seed = await SeedRecord.getOne(req.params.seedId);
+        res.json({
+            seed,
+        })
+    })
+
+    .post('/', async (req, res) => {
+        const newSeed = new SeedRecord(req.body);
+        await newSeed.insert();
+
+        res.json(newSeed);
+    })
+
+    .delete('/:idSeed', async (req, res) => {
+        const seed = await SeedRecord.getOne(req.params.idSeed);
+
+        if (!seed) {
+            throw new ValidationError('There is no such addition to bread.');
+        }
+
+        await seed.delete();
+
+        res.end();
+    })
